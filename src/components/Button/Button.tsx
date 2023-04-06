2a0fdb01820fc7f5193b36df3b7ec49d5c78ca5e
Samanyu <samanyuneelson@gmail.com>
2023-04-06 23:27:17 +0530

    initial whatsapp button property

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,65 @@
 import React from "react";
+import IconButton from "@mui/material/IconButton";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { styled } from "@mui/material/styles";
 
-export interface ButtonProps {
-  label: string;
+const CustomWhatsappIcon = styled(IconButton)`
+  display: block;
+  position: sticky;
+  width: 60px;
+  height: 60px;
+  bottom: 40px;
+  margin-top: -60px;
+  background-color: #128c7e;
+  box-shadow: 2px 2px 5px black;
+  color: white;
+
+  .left {
+    float: left;
+    left: 40px;
+  }
+
+  .right {
+    color: black;
+    float: right;
+    right: 40px;
+  }
+
+  svg {
+    width: 35px;
+    height: 35px;
+  }
+
+  :hover {
+    background-color: #075e54;
+  }
+`;
+
+interface WhatsappProps {
+  whatsappNumber: string;
+  /** position of the button could be "left" or "right". default is right */
+  position?: string;
+  /** distance offset from ether position. Default is 40px */
+  spacing?: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const WhatsappIcon = ({
+  whatsappNumber,
+  position = "right",
+  spacing = "40px"
+}: WhatsappProps) => {
+  const href = `https://wa.me/${whatsappNumber}`;
+  return (
+    <CustomWhatsappIcon
+      sx={{float: position, right: spacing, left: spacing}}
+      color="secondary"
+      aria-label="Contact"
+      size="large"
+      onClick={() => window.open(href)}
+    >
+      <WhatsAppIcon />
+    </CustomWhatsappIcon>
+  );
 };
 
-export default Button;
+export default WhatsappIcon;
